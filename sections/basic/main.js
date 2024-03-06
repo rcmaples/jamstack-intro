f1144677d1d4e69ec144d27edcc5a2846fcfd005
RC Maples <rc@rcmaples.io>
2024-03-06 14:56:37 -0500

    add script

---
+++ sections/basic/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const listRepos = async (username) => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owners&sort=updated`
+  )
+    .then((res) => res.json())
+    .catch((err) => console.err(err));
+
+  const markup = repos
+    .map(
+      (repo) => `
+      <li>
+        <a href="${repo.html_url}">${repo.name}</a>
+        (🥸 ${repo.stargazers_count})
+      </li>
+    `
+    )
+    .join("");
+
+  const content = document.getElementById("content");
+  content.innerHTML = `<ul>${markup}</ul>`;
+};
+
+listRepos("rcmaples");
